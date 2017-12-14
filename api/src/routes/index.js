7e353a1101d0b2ffa10f6e71ba4215d641117736
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-12-14 17:27:01 +0100

    Delete ticket routes + track routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import UserRoutes from './UserRoutes';
 import LessonRoutes from './LessonRoutes';
-import TicketRoutes from './TicketRoutes';
-import TrackRoutes from './TrackRoutes';
 import verify from '../middlewares/AuthMiddleware';
 
 const router = express.Router();
